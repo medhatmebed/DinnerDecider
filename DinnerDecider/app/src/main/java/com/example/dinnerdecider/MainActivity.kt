bab3e865c99924eb1747d2856b5964750e564868
medhatmebed <medhat.mebed@gmail.com>
2020-09-03 21:04:46 -0500

    adding the favorite food to the foodList

--- DinnerDecider/app/src/main/java/com/example/dinnerdecider/MainActivity.kt
+++ DinnerDecider/app/src/main/java/com/example/dinnerdecider/MainActivity.kt
@@ -7,7 +7,7 @@ import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    var foodList = arrayOf("Pizza", "Burger", "Kabab", "KFC", "MAC")
+    var foodList = mutableListOf<String>("Pizza", "Burger", "Kabab", "KFC", "MAC")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -17,8 +17,13 @@ class MainActivity : AppCompatActivity() {
         if (editTxt.text.toString() == "") {
             Toast.makeText(this, "Please enter your food!",
                 Toast.LENGTH_SHORT).show();
+        }else {
+            val str = editTxt.text.toString()
+            if (!foodList.contains(str)) {
+                foodList.add(editTxt.text.toString())
+            }
+            txtView.text = str
         }
-        txtView.text = editTxt.text.toString()
     }
     fun decideBtnPressed(view: View) {
 
